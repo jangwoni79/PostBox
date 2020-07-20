9e6930dc862f93e115953759af08e3d13fd57581
jangwonie <s2018w17@e-mirim.hs.kr>
2020-07-20 18:01:57 +0900

    list/views LetterCreateView, LetterUpdateView, LetterDeleteView

--- list/views.py
+++ list/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+
+from list.models import Letter
 
 
 class LetterListView(ListView):
     model = Letter
+
+class LetterCreateView(CreateView):
+    model = Letter
+    fields = '__all__' #['title', 'contents', 'image']
+    template_name = 'list/letter_create.html'
+    success_url = reverse_lazy('list:list')
+
+class LetterUpdateView(UpdateView):
+    model = Letter
+    fields = '__all__'
+    template_name_suffix = '_update' #drink_update.html
+    success_url = reverse_lazy('list:list')
+
+class LetterDeleteView(DeleteView):
+    model = Letter
+    success_url = reverse_lazy('list:list')
