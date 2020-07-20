860b6897ac29fbfdcd57cd32877bf64b2a5c30d3
jangwonie <s2018w17@e-mirim.hs.kr>
2020-07-20 20:22:29 +0900

    list/urls add_letter, update, delete

--- list/urls.py
+++ list/urls.py
@@ -1,10 +1,13 @@
 
 from django.urls import path
-from list.views import LetterListView
+from list.views import LetterListView, LetterCreateView, LetterUpdateView, LetterDeleteView
 
 app_name = 'list'
 
 urlpatterns = [
     path('', LetterListView.as_view(), name = 'list'),
+    path('add_letter/', LetterCreateView.as_view(), name = 'add_letter'),
+    path('update/<int:pk>/', LetterUpdateView.as_view(), name = 'update'),
+    path('delete/<int:pk>/', LetterDeleteView.as_view(), name = 'delete'),
 ]
 
