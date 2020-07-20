de58646e2d0bc9060b62d2354e378d02998a253f
jangwonie <s2018w17@e-mirim.hs.kr>
2020-07-20 20:28:39 +0900

    .

--- list/models.py
+++ list/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Letter(models.Model):
+    title = models.CharField(max_length=100)
+    contents = models.CharField(max_length=1000)
+    image = models.ImageField(blank=True, upload_to='list/%Y/%m/%d/')
+
+    def __str__(self):
+        return f'제목: {self.title}, 내용: {self.contents}'
